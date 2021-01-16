2dac0127232938e07ff4e1b1bc87eca46dfdc05b
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-16 19:49:21 +0530

    First Commit

---
+++ src/main/java/com/example/userManagement/RestApplicationOfUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.userManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplicationOfUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplicationOfUserManagementApplication.class, args);
+	}
+
+}
