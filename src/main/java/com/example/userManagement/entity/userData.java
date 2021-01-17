efc1ef41ca84b84667a4fd29c4c0d40b5f863c70
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-17 20:39:27 +0530

    added userData class

--- src/main/java/com/example/userManagement/entity/userData.java
+++ src/main/java/com/example/userManagement/entity/userData.java
@@ -1,4 +1,87 @@
 package com.example.userManagement.entity;
 
 public class userData {
+    private int id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String emailID;
+    private long mobileNumber;
+    private String address1;
+    private String address2;
+
+    public userData(int id, String userName, String firstName, String lastName, String emailID, long mobileNumber, String address1, String address2) {
+        this.id = id;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailID = emailID;
+        this.mobileNumber = mobileNumber;
+        this.address1 = address1;
+        this.address2 = address2;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
 }
