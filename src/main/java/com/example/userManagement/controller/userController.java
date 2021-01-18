9320309777b8b3478e3b7a0a83926d37ed98da13
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-18 16:47:24 +0530

    added post and get mapping in controller

---
+++ src/main/java/com/example/userManagement/controller/userController.java
@@ -0,0 +1,28 @@
+package com.example.userManagement.controller;
+
+
+import com.example.userManagement.entity.userData;
+import com.example.userManagement.service.userInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class userController {
+    @Autowired
+    private userInfoService service;
+
+
+    @PostMapping("/adduser")
+    public userData addUser(@RequestBody userData userdata)
+    {
+       return service.saveUserData(userdata);
+    }
+
+    @GetMapping("/userbyID/{id}")
+    public userData findUserbyID(@PathVariable int id)
+    {
+        return service.getuserDatabyID(id);
+    }
+
+    
+}
