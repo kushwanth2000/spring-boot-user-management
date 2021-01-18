22a93aad5cc0788b8f765590e5c0070e756b7ff8
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-18 11:21:10 +0530

    added post method in service

--- src/main/java/com/example/userManagement/service/userInfoService.java
+++ src/main/java/com/example/userManagement/service/userInfoService.java
@@ -1,5 +1,6 @@
 package com.example.userManagement.service;
 
+import com.example.userManagement.entity.userData;
 import com.example.userManagement.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Service;
 public class userInfoService {
 
     @Autowired
-    private userRepository repository;
+    private userRepository repository;          // injecting repository into userInfoService
 
+    public userData saveUserData(userData userdata ) {
+        return repository.save(userdata);
+    }
 }
