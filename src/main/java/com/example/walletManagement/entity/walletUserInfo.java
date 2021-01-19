53b26cf15c674b1d608fa259b34997a97a354afc
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-19 20:37:47 +0530

    added getters and setters in walletUserInfo class

--- src/main/java/com/example/walletManagement/entity/walletUserInfo.java
+++ src/main/java/com/example/walletManagement/entity/walletUserInfo.java
@@ -1,2 +1,38 @@
-package com.example.walletManagement.entity;public class walletUserInfo {
+package com.example.walletManagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class walletUserInfo {
+
+    @Id
+    @Column(name = "phoneNumber")
+    private long phoneNumber;
+    @Column(name = "balance")
+    private int balance;
+
+    public walletUserInfo(long phoneNumber, int balance)
+    {
+        this.phoneNumber=phoneNumber;
+        this.balance=balance;
+    }
+
+    public walletUserInfo(){}  // default constructor
+
+                                                      // getters and setters
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
