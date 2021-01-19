5d4019fad648abcfda31b97f47983d5cfb9c46d5
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-19 09:22:53 +0530

    added validations for post request

--- src/main/java/com/example/userManagement/controller/userController.java
+++ src/main/java/com/example/userManagement/controller/userController.java
@@ -6,6 +6,8 @@ import com.example.userManagement.service.userInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class userController {
     @Autowired
@@ -13,10 +15,20 @@ public class userController {
 
 
     @PostMapping("/adduser")
-    public userData addUser(@RequestBody userData userdata)
+    public String addUser(@RequestBody userData userdata)
     {
-       return service.saveUserData(userdata);
+        List<userData> user_email = service.findByEmailID(userdata.getEmailID());
+        List<userData> user_username = service.findbyUserName(userdata.getUserName());
+        List<userData> user_mobile_number = service.findbyMobileNumber(userdata.getMobileNumber());
+
+                                    // checking for existing Users
+        if(!user_email.isEmpty())   {return "User with same emailID already exists";}
+        else if (!user_username.isEmpty() ) {return "User with same userName already exists";}
+        else if (!user_mobile_number.isEmpty()) {return "User with same mobileNumber already exists";}
+        else  {service.saveUserData(userdata);}
+        return "User saved";
     }
+
 
     @GetMapping("/userbyID/{id}")
     public userData findUserbyID(@PathVariable int id)
