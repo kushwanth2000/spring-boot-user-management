b0111d65bf0389ce33e2cf3a9136f272b2958624
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-20 16:33:00 +0530

    added save transaction method in service

--- src/main/java/com/example/transactionManagement/service/transactionService.java
+++ src/main/java/com/example/transactionManagement/service/transactionService.java
@@ -1,7 +1,33 @@
 package com.example.transactionManagement.service;
 
 
+import com.example.transactionManagement.entity.transaction;
+import com.example.transactionManagement.repository.transactionRepository;
+import com.example.userManagement.entity.userData;
+import com.example.userManagement.repository.userRepository;
+import com.example.walletManagement.entity.walletUserInfo;
+import com.example.walletManagement.repository.walletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class transactionService {
+    @Autowired
+    private walletRepository walletrepository;
+    @Autowired
+    private userRepository userrepository;
+    @Autowired
+    private transactionRepository transactionrepository;
 
+    public transaction saveTransactiondata(transaction usertransaction)
+    {
+        return transactionrepository.save(usertransaction);
+    }
+
+    
+
+    public List<walletUserInfo> findByPhoneNumber(long phoneNumber){return walletrepository.findByPhoneNumber(phoneNumber);}
 }
+
