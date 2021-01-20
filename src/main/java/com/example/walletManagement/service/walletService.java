763cbffc1e1b06b9557f820153de7fd466638cc1
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-20 12:08:53 +0530

    added post method in service class

--- src/main/java/com/example/walletManagement/service/walletService.java
+++ src/main/java/com/example/walletManagement/service/walletService.java
@@ -1,15 +1,28 @@
 package com.example.walletManagement.service;
 
 
+import com.example.userManagement.entity.userData;
+import com.example.userManagement.repository.userRepository;
+import com.example.walletManagement.entity.walletUserInfo;
 import com.example.walletManagement.repository.walletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class walletService {
 
     @Autowired
-    private walletRepository repository;
+    private walletRepository walletrepository;
+    @Autowired
+    private userRepository userrepository;
 
+    public walletUserInfo saveWallet(walletUserInfo walletuserinfo)
+    {
+       return walletrepository.save(walletuserinfo);
+    }
 
+    public List<userData> findByMobileNumber(long mobilenumber){return userrepository.findByMobileNumber(mobilenumber);}
+    public List<walletUserInfo> findByPhoneNumber(long phoneNumber){return walletrepository.findByPhoneNumber(phoneNumber);}
 }
