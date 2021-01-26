65db4d3d0f06562a158fcb6cdc9bd26abf884b5b
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-26 15:02:50 +0530

    added server ports for kafka and ES in app.props

--- src/main/java/com/example/RestApplicationOfUserManagement.java
+++ src/main/java/com/example/RestApplicationOfUserManagement.java
@@ -1,10 +1,17 @@
 package com.example;
 
+import com.example.TransactionManagementElastic.Repository.transactionElasticSearchRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
 public class RestApplicationOfUserManagement {
 
 	public static void main(String[] args) {
