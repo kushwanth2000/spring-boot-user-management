6ecbb58f9253699da1e9ffd372a08f2f41520440
kushwanth2000 <Kushwanth2000@gmail.com>
2021-01-26 14:48:37 +0530

    added Kafka Config in TransElastic

---
+++ src/main/java/com/example/TransactionManagementElastic/KafkaConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.TransactionManagementElastic;
+
+
+import com.example.TransactionManagementElastic.Model.TransactionElastic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean       // for producer serialization
+     public ProducerFactory<String , TransactionElastic> producerFactory(){
+        Map<String, Object> config= new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory(config);
+     }
+
+     @Bean         // kafka message template
+    public KafkaTemplate<String, TransactionElastic> kafkaTemplate(){
+        return new KafkaTemplate<String,TransactionElastic>(producerFactory());
+     }
+
+
+
+
+
+}
